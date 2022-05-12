2281ad1c270f8f1748f0e42b0890c736322b2367
greatstep93 <greatstep93@gmail.com>
2022-05-12 08:58:02 +0400

    Задача 1.1.4 Соединение с БД через Hibernate;

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
         UserServiceImpl userService = new UserServiceImpl();
-
+        userService.dropUsersTable();
         userService.createUsersTable();
 
         userService.saveUser("Дмитрий", "Ткаченко", (byte) 29);
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,8 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
 
 import java.util.List;
 
@@ -9,34 +11,84 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
-
     @Override
     public void createUsersTable() {
-
+        try (Session session = Util.getSession()) {
+            session.getTransaction().begin();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT,"
+                    + "firstName VARCHAR(255),lastName VARCHAR(255), age INT, PRIMARY KEY (id));").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("Ошибка создания таблицы");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        try (Session session = Util.getSession()) {
+            session.getTransaction().begin();
+            session.createSQLQuery("DROP TABLE users").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception ignore) {
 
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = Util.getSession()) {
+            session.getTransaction().begin();
+            session.persist(new User(name, lastName, age));
+            session.getTransaction().commit();
+            System.out.println("User с именем " + name + " " + lastName + " сохранен в базу данных");
+        } catch (Exception e) {
+            System.out.println("Ошибка сохранения");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try (Session session = Util.getSession()) {
+            session.getTransaction().begin();
 
+            session.createQuery("DELETE FROM User WHERE id = " + id).executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("Ошибка удаления пользователя по id");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = null;
+        try (Session session = Util.getSession()) {
+            session.getTransaction().begin();
+
+            userList = session.createQuery("SELECT a FROM User a", User.class).getResultList();
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+
+            System.out.println("Ошибка получения списка пользователей");
+        }
+        return userList;
     }
 
     @Override
     public void cleanUsersTable() {
+        try (Session session = Util.getSession()) {
+            session.getTransaction().begin();
 
+            session.createQuery("DELETE FROM User").executeUpdate();
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+
+            System.out.println("Ошибка очистки таблицы");
+        }
     }
 }
